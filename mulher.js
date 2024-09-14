55e4a7371128818f49ae13badfbb586dc530b462
Letycia <lelocha19@gmail.com>
2024-09-14 17:52:09 -0300

    atualizar

---
+++ mulher.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const router = express.Router()
+
+const app = express()
+const porta = 3333;
+
+function mostraMulher(request, response) {
+    response.json({
+        nome:"Marisa Monte",
+        imagem: 'https://akamai.sscdn.co/uploadfile/letras/fotos/f/7/b/a/f7bae22807ab335f4a21677ef8e5c18f.jpg',
+        minibio: 'Cantora e compositora Brasileira',    })
+}
+
+function mostraPorta (){
+    console.log("Servidor criado e rodando na porta ", porta)
+};
+app.use(router.get('/mulheres', mostraMulher))
+app.listen (porta, mostraPorta );
+
